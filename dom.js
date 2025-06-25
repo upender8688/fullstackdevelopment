1c9da5d673867a7f3084d8a5d5c8157324e85310
upender8688 <konapakalaupender@gmail.com>
2025-06-25 15:18:44 +0530

    DOM

---
+++ dom.js
@@ -0,0 +1,19 @@
+document.title="DOM in javascript";
+document.getElementById("a").innerHTML="MRECT ";
+document.body.style.backgroundColor="lightRed";
+document.body.style.textAlign="center";
+document.getElementById("para").style.color="red";
+document.getElementById("para").style.fontSize="30px";
+document.getElementById("ds").style.color="blue";
+document.getElementsByClassName("b")[0].style.color="brown";
+document.getElementsByClassName("ds")[0].style.color="green";
+ document.getElementsByClassName("ds")[1].style.color=" blue";
+ document.getElementsByClassName("ds")[2].style.color="orange";
+ document.getElementsByClassName("ds")[0].style.fontSize="30px";
+ document.getElementsByClassName("ds")[1].style.fontSize="30px";
+ document.getElementsByClassName("ds")[2].style.fontSize="30px";
+ 
+ 
+ 
+ 
+ 
