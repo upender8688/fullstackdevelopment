cc951a817d022b64bed06306a2041cbad7830518
upender8688 <konapakalaupender@gmail.com>
2025-07-04 12:13:02 +0530

    base version

---
+++ ajio.js
@@ -0,0 +1,22 @@
+ const image = document.querySelector('aab');
+image.addEventListener('mousemove', () => {
+    image.style.transform = 'scale(1.1)';  
+});
+image.addEventListener('mouseleave', () => {
+    image.style.transform = 'scale(1)';  
+});
+  const a = document.querySelector('aab');
+a.addEventListener('mousemove', () => {
+a.style.transform = 'scale(1.1)';  
+});
+a.addEventListener('mouseleave', () => {
+    a.style.transform = 'scale(1)';  
+});
+  const b = document.querySelector('aa');
+b.addEventListener('mousemove', () => {
+    b.style.transform = 'scale(1.1)';  
+});
+b.addEventListener('mouseleave', () => {
+    b.style.transform = 'scale(1)';  
+});
+ 
